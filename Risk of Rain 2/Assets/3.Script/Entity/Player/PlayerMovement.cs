d6da1c91614784c37a66cd0484783955fabcb63c
00ill <cyi1769@naver.com>
2023-05-17 14:22:55 +0900

    [fix] : 플레이어 이동 스크립트 수정

--- Risk of Rain 2/Assets/3.Script/Entity/Player/PlayerMovement.cs
+++ Risk of Rain 2/Assets/3.Script/Entity/Player/PlayerMovement.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    //플레이어 컴포넌트
     private Animator _playerAnimator;
     private Rigidbody _playerRigidbody;
     private PlayerInput _playerInput;
 
+    //플레이어 스테이터스
     private float _moveSpeed = 5f;
     private float _rotateSpeed = 270;
-
+    [SerializeField]
+    private float _jumpForce = 2f;
     private void Awake()
     {
         TryGetComponent(out _playerAnimator);
@@ -33,20 +36,23 @@ public class player : MonoBehaviour
     private void Rotate()
     {
         Quaternion _rotation = Quaternion.Euler(new Vector3(0, _playerInput.Rotate * Time.deltaTime * _rotateSpeed, 0));
-        
+
         _playerRigidbody.rotation *= _rotation;
     }
-
-    public void ReturnIdle()
+    /// <summary>
+    /// 플레이어 점프 메서드
+    /// </summary>
+    public void Jump()
     {
-        _playerAnimator.SetBool("Jump", false);
+        _playerRigidbody.AddForce(Vector3.up * _jumpForce);
+        _playerAnimator.SetBool("Jump", true);
+        Debug.Log("점프");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Ground"))
         {
-            Debug.Log("충돌");
             _playerAnimator.SetBool("Jump", false);
         }
     }

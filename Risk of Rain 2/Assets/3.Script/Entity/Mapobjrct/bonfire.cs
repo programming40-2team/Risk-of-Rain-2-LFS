9f8eaa609765151be7c26cd0d95dbfc7f1a8bffd
이성준 <132257323+PowerJu@users.noreply.github.com>
2023-05-22 17:55:10 +0900

    [Add] : Geep몬스터 구현 애니메이션 구현 [Fix]: 몬스터 움직임 수정

---
+++ Risk of Rain 2/Assets/3.Script/Entity/Mapobjrct/bonfire.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bonfire : MonoBehaviour
+{
+    public float jumpForce = 5f; // 점프 힘
+
+    private Rigidbody playerRigidbody; // 플레이어의 Rigidbody 컴포넌트
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("test"))
+        {
+            JumpToCube2();
+        }
+    }
+
+    private void JumpToCube2()
+    {
+        // cube2로의 점프 힘을 적용
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}

abf2e03438c5f052d1960a50e0067e3bb807a6f3
최영일 <cyi1769@naver.com>
2023-05-22 11:26:20 +0900

    [Feat] : 대쉬 추가

--- Risk of Rain 2/Assets/3.Script/Entity/Player/PlayerAttack.cs
+++ Risk of Rain 2/Assets/3.Script/Entity/Player/PlayerAttack.cs
@@ -11,18 +11,29 @@ public class PlayerAttack : MonoBehaviour
     public bool IsAttacking;
     public int AttackCount = 1;
 
+    [SerializeField] private float _dashDistance;
+
+    private Rigidbody _playerRigidbody;
     private void Awake()
     {
         TryGetComponent(out _playerAnimator);
+        TryGetComponent(out _playerRigidbody);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash_co());
+        }
+    }
     public void Attack1()
     {
         IsAttacking = true;
         _playerAnimator.SetBool("Attack1", true);
         AttackCount++;
     }
-    public void Attack2() 
+    public void Attack2()
     {
         IsAttacking = true;
         _playerAnimator.SetBool("Attack2", true);
@@ -35,7 +46,7 @@ public class PlayerAttack : MonoBehaviour
         _playerAnimator.SetBool("Attack3", true);
         AttackCount++;
         StopCoroutine(RunningCoroutine);
-        
+
     }
     public IEnumerator AttackTimeCheck_co()
     {
@@ -50,7 +61,7 @@ public class PlayerAttack : MonoBehaviour
     {
         IsAttacking = false;
         RunningCoroutine = StartCoroutine(AttackTimeCheck_co());
-       
+
     }
     private void EndAttack()
     {
@@ -59,5 +70,34 @@ public class PlayerAttack : MonoBehaviour
         _playerAnimator.SetBool("Attack1", false);
         AttackCount = 0;
         IsAttacking = false;
+    }
+
+    /// <summary>
+    /// 대쉬 거리는 고정일 거고, 무적판정넣고, 경로에 존재하는 몬스터에게 마비와 데미지,
+    /// 적을 맞히면 한 번 더 사용가능 최대 3회
+    /// prep - loop - exit
+    /// 무적판정과, 몬스터에게 데미지
+    /// --플레이어를 트리거로 바꾼다. ontriggerEnter - 마비(나중예), 데미지
+    /// </summary>
+    /// <returns></returns>
+
+    private IEnumerator Dash_co()
+    {
+        yield return null;
+        Vector3 _dir = transform.forward;
+        _playerRigidbody.MovePosition(transform.position + _dir * _dashDistance);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("충돌2");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Monster"))
+        {
+            other.GetComponent<Entity>().OnDamage(10);
+        }
     }
 }

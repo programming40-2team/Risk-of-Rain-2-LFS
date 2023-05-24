82ee89d0f75a46e27eb566d2a2872798dff4723c
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-05-24 10:59:58 +0900

    [Fix]: 걷는 애니메이션 수정 완료

--- Risk of Rain 2/Assets/3.Script/Entity/Monster/BossMonster/WalkState.cs
+++ Risk of Rain 2/Assets/3.Script/Entity/Monster/BossMonster/WalkState.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class WalkState : StateMachineBehaviour
 {
     private Transform _beetleQueenTransForm;
-    private Transform _whereGo;
     private BeetleQueen _beetleQueen;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
@@ -18,24 +17,12 @@ public class WalkState : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        _beetleQueenTransForm.position += _beetleQueenTransForm.forward * _beetleQueen.MoveSpeed * Time.deltaTime;
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //_beetleQueen.transform.position = _whereGo.position;
+
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }

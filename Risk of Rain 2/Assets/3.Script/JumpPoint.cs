8382827aa9ffdfe001acc349f61606c3bb25a63e
DESKTOP-40OSJNT\sse79 <sse7946@naver.com>
2023-05-25 10:12:24 +0900

    [Feat] : 보스제단 랜덤 위치 생성. 보스제단 접근시 보스 소환 비콘 활성화 및 E키를 누르면 보스 소환. 보스제단 파티클.

--- Risk of Rain 2/Assets/3.Script/JumpPoint.cs
+++ Risk of Rain 2/Assets/3.Script/JumpPoint.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class JumpPoint : MonoBehaviour
 {
-    [SerializeField] GameObject jumpObj;  //점프하는 대상(인스펙터 창에서 넣을 필요없음. 아래 코드로 설정. 이하 점프오브젝트)
-    [SerializeField] GameObject goalPoint;  //점프 후 목적지(인스펙터 창에서 넣어야함)
+    [SerializeField] GameObject jumpObj;  //점프하는 대상 (인스펙터 창에서 넣을 필요없음. 아래 코드로 설정. 이하 점프오브젝트)
+    [SerializeField] GameObject highPoint;  //점프 후 최대 높이 지점 (인스펙터 창에서 넣어야함)
+    [SerializeField] GameObject goalPoint;  //점프 후 목적지 (인스펙터 창에서 넣어야함)
 
     [Header("점프속도 조절")]
-    [SerializeField] [Range(0.001f, 1f)] float jumpSpeed = 0.5f;    //목적지까지 이동하는 속도
+    [SerializeField] [Range(0.001f, 1f)] float jumpSpeed = 0.001f;    //목적지까지 이동하는 속도
 
-    bool isJumping = false;     //점프오브젝트의 점프 상태를 확인하기 위함
     Rigidbody jumpRigidbody;      //점프동안 Rigidbody Gravity를 없애기 위함.
 
 
@@ -20,46 +20,43 @@ public class JumpPoint : MonoBehaviour
         jumpObj = other.gameObject;     //접촉하는 게임오브젝트를 점프오브젝트로 설정한다.
         jumpRigidbody = jumpObj.GetComponent<Rigidbody>();
         
-        StartCoroutine(JumpGear());     //접촉하면 점프기어 코루틴 동작
+        StartCoroutine(JumpGear_co());     //접촉하면 점프기어 코루틴 동작
         Debug.Log("코루틴 시작");
-
     }
 
     
 
-    IEnumerator JumpGear()
+    IEnumerator JumpGear_co()
     {
-        isJumping = true;
-        Vector3 goalPos = goalPoint.transform.position;
-        
-        while (Vector3.SqrMagnitude(jumpObj.transform.position - goalPos)>= 0.05f)
+        Vector3 goalPos = goalPoint.transform.position;     //도착지점, 인스펙터창에서 추가한 오브젝트의 위치를 받아온다.
+        Vector3 highPos = highPoint.transform.position;     //최대 높이지점
+
+        //while문과 SqrMagnitude 메소드를 통해서 점프하는 오브젝트와 도착지점의 거리를 확인하고, 그 거리가 0.05f보다 멀면 점프 오브젝트의 중력을 false로 만든다
+
+        while (Vector3.SqrMagnitude(jumpObj.transform.position - highPos) >= 0.05f)
         {
-            yield return null;
             jumpRigidbody.useGravity = false;
-            jumpObj.transform.position = Vector3.Slerp(jumpObj.transform.position, goalPos, jumpSpeed);
+            jumpObj.transform.position = Vector3.Slerp(jumpObj.transform.position, highPos, jumpSpeed);
+
+            if (Vector3.SqrMagnitude(jumpObj.transform.position - highPos) <= 0.05f)
+            {
+                Debug.Log("중간");
+                jumpRigidbody.useGravity = true;
+                
+                //while (Vector3.SqrMagnitude(jumpObj.transform.position - goalPos) >= 0.05f)
+                //{
+                //    Debug.Log("마지막");
+                //    //*yield return null;
+                //    //*jumpRigidbody.useGravity = false;
+                //    //Slerp + transform.position을 통해 포물선으로 이동하며 도착지점까지 이동한다. 점프 오브젝트와 도착지점의 거리가 0.05f 이상이면 코드를 반복해서 이동하게 된다.
+                //    jumpObj.transform.position = Vector3.Slerp(jumpObj.transform.position, goalPos, jumpSpeed);
+                //}
+                //jumpObj.transform.position = goalPos;
+                //*jumpRigidbody.useGravity = true;
+                yield return null;
+            }
         }
-        jumpObj.transform.position = goalPos;
-        jumpRigidbody.useGravity = true;
-        isJumping = false;
-
-        //    while (isJumping)    //목표지점에 도착할때까지 반복한다
-        //    {
-        //        yield return null;
-        //        jumpRigidbody.useGravity = false;     //부드러운 동작을 위해 점프 동안, 점프 오브젝트의 중력을 없앤다.
-        //        Vector3 goalPos = goalPoint.transform.position;
-        //        jumpObj.transform.position = Vector3.Slerp(jumpObj.transform.position, goalPos, jumpSpeed);     //SLerp를 이용하여 포물선을 그리며 목적지에 도착하게 된다.
-        //        
-        //        float distance = Vector3.Distance(jumpObj.transform.position, goalPos);
-        //        if (distance < 0.03f)
-        //        {
-        //            Debug.Log("된다");
-        //            jumpRigidbody.useGravity = true;
-        //            isJumping = false;
-        //        }
-        //    }
     }
-
-
 
 
 }

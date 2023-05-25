006f4ffe1d974d9d3e928d89bda345cd90d31468
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-05-25 09:24:57 +0900

    [Refactor]: TODO 추가

--- Risk of Rain 2/Assets/3.Script/Entity/Monster/BossMonster/RangeBomb.cs
+++ Risk of Rain 2/Assets/3.Script/Entity/Monster/BossMonster/RangeBomb.cs
@@ -12,8 +12,9 @@ public class RangeBomb : MonoBehaviour
     private IEnumerator DeleteRangeBomb_co()
     {
         yield return new WaitForSeconds (5f);
-        Destroy(gameObject);
-        //폭발
-        //띄우기 / 데미지는 400%
+        // 폭발
+        // 띄우기 / 데미지는 400%
+        // 마지막에 Destroy(gameObject);
+        // 얘도 오브젝트풀링 할 수는 있는데 해야 하나..? 해야 한다면 바꾸기 / 바꾸는건 쉽잖아
     }
 }

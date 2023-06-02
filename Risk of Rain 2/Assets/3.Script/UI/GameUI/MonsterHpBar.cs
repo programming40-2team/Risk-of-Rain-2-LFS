8b9fe81007ad07d46ac12deb597afc80fa11da28
Cowhydra <126942402+Cowhydra@users.noreply.github.com>
2023-06-02 09:28:42 +0900

    [fix] GameUI 폰트 사이즈 수정, 몬스터 HPBar 코드 몇줄 추가
    폰트 사이즈 수정, 몬스터 HPBar 코드 몇줄 추가

--- Risk of Rain 2/Assets/3.Script/UI/GameUI/MonsterHpBar.cs
+++ Risk of Rain 2/Assets/3.Script/UI/GameUI/MonsterHpBar.cs
@@ -5,28 +5,41 @@ using UnityEngine.UI;
 
 public class MonsterHpBar : UI_Base
 {
+    Entity _myentity;
     enum Sliders
     {
         HpBar,
     }
-    public override void Init()
+
+    private void Awake()
     {
-       
-
+        Init();   
     }
-
-    // Start is called before the first frame update
-    void Start()
+    public override void Init()
     {
         Bind<Slider>(typeof(Sliders));
         Debug.Log("몬스터 체력 컴포넌트 가져와서 value 값해줘야함");
+
+    }
+
+    private void OnEnable()
+    {
+       if(gameObject.transform.root.TryGetComponent(out Entity entity))
+        {
+            _myentity = entity;
+        }
+        else
+        {
+            Debug.Log($"{gameObject.transform.root} 에서 Entity를 가져올 수 없음");
+        }
+    
     }
 
     void Update()
     {
         Transform parent = transform.parent;
         transform.position=parent.position+Vector3.up*(parent.GetComponent<Collider>().bounds.size.y);
-
+        Get<Slider>((int)Sliders.HpBar).value = _myentity.Health / _myentity.MaxHealth;
         //부모의 체력을 끌고와서 업데이트 해주면됨
     }
 }

78d171cd7bd7101cadf2bd817f9aeddafe5ea559
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-06-07 15:32:58 +0900

    [Fix]: 보스 소환 한 마리만 되도록 수정

--- Risk of Rain 2/Assets/3.Script/Map/Altar.cs
+++ Risk of Rain 2/Assets/3.Script/Map/Altar.cs
@@ -12,6 +12,8 @@ public class Altar : MonoBehaviour
     [SerializeField] Transform _spawnPoint;
     [SerializeField] ParticleSystem _bossRazer;
 
+    private bool _isSpawn = false;
+
     private void Awake()
     {
         _renderer = this.GetComponent<Renderer>();
@@ -52,11 +54,16 @@ public class Altar : MonoBehaviour
             {
                 Debug.Log("입력 및 보스 생성");
 
-                Instantiate(_bossPrefab, bossSpawnPoint.position, bossSpawnPoint.rotation);  //제단에서 e키를 누르면 보스가 소환될 지점
-                BossRazer();
+                if(!_isSpawn)
+                {
+                    Instantiate(_bossPrefab, bossSpawnPoint.position, bossSpawnPoint.rotation);  //제단에서 e키를 누르면 보스가 소환될 지점
+                    BossRazer();
 
-                //보스가 생성되면 게임의 현재 상태를 ActiveTelePort로 바꾸어 관련 UI들 갱신!
-                Managers.Game.GameState = Define.EGameState.ActiveTelePort;
+                    //보스가 생성되면 게임의 현재 상태를 ActiveTelePort로 바꾸어 관련 UI들 갱신!
+                    Managers.Game.GameState = Define.EGameState.ActiveTelePort;
+                    _isSpawn = true;
+                }
+
             }
         }
     }

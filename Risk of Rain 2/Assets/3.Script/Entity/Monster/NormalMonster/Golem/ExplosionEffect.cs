bf54cd7eef02e76545ddf4e0e02e11710b98b52a
최영일 <cyi1769@naver.com>
2023-06-07 09:57:35 +0900

    no message

---
+++ Risk of Rain 2/Assets/3.Script/Entity/Monster/NormalMonster/Golem/ExplosionEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ExplosionEffect : MonoBehaviour
+{
+    [SerializeField] private Golem _golem;
+    private readonly float _explosionCoefficents = 2.5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Entity>().OnDamage(_golem.Damage * _explosionCoefficents);
+        }
+    }
+}
